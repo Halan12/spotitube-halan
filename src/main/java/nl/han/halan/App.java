28b2e4fbab68862a061bd2870fd88169ce1dac7b
Halan <H.AlSaleh@student.han.nl>
2021-05-04 15:30:27 +0200

    not

--- src/main/java/nl/han/halan/App.java
+++ src/main/java/nl/han/halan/App.java
@@ -5,6 +5,6 @@ package nl.han.halan;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("not Hello World!");
     }
 }
